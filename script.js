eec3afb109b0782583df4448cb6736281bef656c
t510599 <t510599@gmail.com>
2017-12-04 00:03:05 +0800

    Dev 1.1

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Dev 1.1
+// (c) Copyright 2015-2017 Stone App Technology Studio.
+
 // initialize the variable
 var html = ace.edit("html");
 var css = ace.edit("css");
@@ -31,6 +34,11 @@ update();
 html.setValue(content);
 load();
 // finish
+
+// welcome
+if (!localStorage.getItem("visited")) {
+    welcome();
+}
 
 function load() {
     dimmer.classList.add("active");
@@ -69,4 +77,25 @@ function update() {
     document.querySelector('a[data-tab="html"]').addEventListener("click", function(){ html.renderer.updateFull(); });
     document.querySelector('a[data-tab="css"]').addEventListener("click", function(){ css.renderer.updateFull(); });
     document.querySelector('a[data-tab="javascript"]').addEventListener("click", function(){ js.renderer.updateFull(); });
+}
+
+function setStorage() {
+    localStorage.setItem('visited','true');
+}
+
+function welcome() {
+    swal({
+        html: '<h1 class="ts center aligned header">歡迎!第一次使用!</h1>',
+        type: "success"
+    });
+    setStorage();
+}
+
+function info() {
+    swal({
+        type: "info",
+        html: '<h2 class="ts center aligned header">About</h2>'+'<a target="_blank" href="https://tocas-ui.com/">TocasUI</a>'+' / '+'<a target="_blank" href="https://ace.c9.io/">Ace Editor</a>'
+              +'<p>(c) Copyright 2015-2017 Stone App Technology Studio.</p>',
+        showConfirmButton: false
+    });
 }
